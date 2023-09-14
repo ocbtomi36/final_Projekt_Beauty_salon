372c7bc22f087678a20dcc1249014406b59233dc
ocbtomi36 <ocbtomi36@outlook.hu>
2023-09-14 20:05:24 +0200

    main.php is modified

--- protected/content/main.php
+++ protected/content/main.php
@@ -1,1 +1,44 @@
-<h2>Ez a main.php</h2>
+<section id="about" class="icons bg-light">
+            <div class="flex-items">
+                <div>
+                    <i class="fas fa-store fa-2px"></i>
+                    <div>
+                        <h3>Our Shop in Budapest</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, debitis quasi.</p>
+                    </div>
+                </div>
+                <div>
+                    <i class="fas fa-store fa-2px"></i>
+                    <div>
+                        <h3>Our Shop in Debrecen</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, debitis quasi.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="ourteam" class="icons bg-light">
+            <div class="flex-items">
+                <div>
+                    <i class="fas fa-store fa-2px"></i>
+                    <div>
+                        <h3>Our Shop in Budapest</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, debitis quasi.</p>
+                    </div>
+                </div>
+                <div>
+                    <i class="fas fa-store fa-2px"></i>
+                    <div>
+                        <h3>Our Shop in Debrecen</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, debitis quasi.</p>
+                    </div>
+                </div>
+                <div>
+                    <i class="fas fa-store fa-2px"></i>
+                    <div>
+                        <h3>Our Shop in Debrecen</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, debitis quasi.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
